c4e49cba0eecdc53db44106bd7f39e9a6682cb11
Naimur53 <naimurrhman53@gmail.com>
2021-10-31 23:34:00 +0600

    done

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div>
             <section className='banner-container'>
-                <video src={video} autoPlay muted loop></video>
+                <video src={video} autoPlay muted playsinline loop></video>
                 <h2>Its Hash<br />  <span> Tour</span> <small className='fs-3 mt-4'>The best site for tour checkOut <a className='text-warning' href="#service">our service</a></small> </h2>
             </section>
         </div>
